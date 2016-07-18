bb7d8c42da4f33a9a091c88070d6e66925084bdf
Byron McMullen <byronsm@gmail.com>
2016-07-17 21:18:19 -0700

    lesson  17 middleware

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router';
 import * as actions from '../actions';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
-import { fetchTodos } from '../api';
 
 class VisibleTodoList extends Component {
 
@@ -19,9 +18,8 @@ class VisibleTodoList extends Component {
   }
 
   fetchData() {
-    const { filter, receiveTodos } = this.props;
-    fetchTodos(this.props.filter)
-      .then(todos => receiveTodos(filter, todos));
+    const { filter, fetchTodos } = this.props;
+    fetchTodos(filter);
   }
 
   render() {
@@ -37,7 +35,7 @@ class VisibleTodoList extends Component {
 
 VisibleTodoList.propTypes = {
   filter: PropTypes.oneOf(['all', 'active', 'completed']).isRequired,
-  receiveTodos: PropTypes.func.isRequired,
+  fetchTodos: PropTypes.func.isRequired,
   toggleTodo: PropTypes.func.isRequired,
 };
 
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,29 +1,46 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
+const logger = (store) => (next) => {
+  /* eslint-disable no-console */
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
 
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state', 'color: green', store.getState());
     console.groupEnd(action.type);
+    /* eslint-enable no-console */
     return returnValue;
   };
 };
 
+const promise = () => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch); // eslint-disable-line no-param-reassign
+  });
+};
+
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
+
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
