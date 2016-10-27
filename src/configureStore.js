c4ad4c54d2c4ad2750ff8ae3db8a2beb9ca5cc41
Byron McMullen <byronsm@gmail.com>
2016-10-26 20:41:41 -0700

    Lesson 22
    Asynchronous actions with thunks

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
+const thunk = (store) => (next) => (action) => {
+  if (typeof action === 'function') {
+    action(store.dispatch);
+  } else {
+    next(action);
+  }
+};
+
 const configureStore = () => {
-  const middlewares = [promise];
+  const middlewares = [thunk];
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
   }
